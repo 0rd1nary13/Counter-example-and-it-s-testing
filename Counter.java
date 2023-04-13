901eead534607156eb9e6d92322a9c7b14b00aa5
Vincent Xayasak <xayasakvincent@gmail.com>
2023-04-13 15:49:24 -0700

    Add files via upload

---
+++ Counter.java
@@ -0,0 +1,118 @@
+
+/**
+ * The counter class implements a counter that will roll over to the initial
+ * value when it hits the maximum value.
+ * 
+ * @author Charles Hoot 
+ * @version 4.0
+ */
+public class Counter
+{
+    // PUT PRIVATE DATA FIELDS HERE
+    private int min;
+    private int max;
+    private int val;
+
+    /**
+     * The default constructor for objects of class Counter.  Minimum is 0 and the maximum
+     * is the largest possible integer.
+     */
+    public Counter()
+    {
+        // ADD CODE FOR THE CONSTRUCTOR
+        this.min = 0;
+        this.max = Integer.MAX_VALUE; 
+        this.val = 0;
+    }
+    
+    
+    /**
+     * The alternate constructor for objects of class Counter.  The minimum and maximum values are given as parameters.
+     * The counter starts at the minimum value.
+     * @param min The minimum value that the counter can have
+     * @param max The maximum value that the counter can have
+     * */
+    public Counter(int min, int max)
+    {
+        // ADD CODE FOR THE ALTERNATE CONSTRUCTOR
+        this.min = min;
+        this.max = max;
+        this.val = min;
+    }
+    
+    /**
+     * Determine if two counters are in the same state
+     *
+     * @param  otherObject   the object to test against for equality
+     * @return     true if the objects are in the same state
+     */
+    public boolean equals(Object otherObject)
+    {
+        if (otherObject instanceof Counter)
+        {
+            // YOUR CODE GOES HERE
+            return true;
+        }
+        return false;
+    }
+    
+    
+
+    /**
+     * Increases the counter by one
+     */
+    public void increase()
+    {
+        // ADD CODE TO INCREASE THE VALUE OF THE COUNTER
+        this.val++;
+    }
+ 
+ 
+     /**
+     * Decreases the counter by one
+     */
+    public void decrease()
+    {
+        // ADD CODE TO INCREASE THE VALUE OF THE COUNTER
+        this.val--;
+    }
+    
+    /**
+     * Get the value of the counter
+     *
+     * @return     the current value of the counter
+     */
+    public int value()
+    {
+        // CHANGE THE RETURN TO GIVE THE CURRENT VALUE OF THE COUNTER
+        return this.val;
+    }
+    
+    
+    /**
+     * Accessor that allows the client to determine if the counter
+     *             rolled over on the last count
+     *
+     * @return     true if the counter rolled over
+     */
+    public boolean rolledOver()
+    {
+        // CHANGE THE RETURN TO THE ROLLOVER STATUS OF THE COUNTER
+        if (this.val == this.max || this.val == this.min) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Override the toString method to provide a more informative
+     * description of the counter
+     *
+     * @return     a descriptive string about the object
+     */
+    public String toString()
+    {
+        // CHANGE THE RETURN TO A DESCRIPTION OF THE COUNTER
+        return "A Counter object that keeps track of the amount of clicks as values, incrementing or decrementing.";		
+    }
+}

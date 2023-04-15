057926fe1be9edb5ad56391b8c2aefd9cac4cd91
Jager <46833038+0rd1nary13@users.noreply.github.com>
2023-04-15 13:37:38 -0700

    Update CounterInitializationException.java

--- CounterInitializationException.java
+++ CounterInitializationException.java
@@ -1,2 +1,7 @@
-public class CounterInitializationException {
+//a runtime exception that signals that the counter could not be created
+
+public class CounterInitializationException extends RuntimeException{
+    public CounterInitializationException(String message){
+        super(message);
+    }
 }

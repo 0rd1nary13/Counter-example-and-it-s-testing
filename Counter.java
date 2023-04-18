de7bf690794a6c330b1eb52418083d3fade928d7
Vincent Xayasak <xayasakvincent@gmail.com>
2023-04-17 23:06:16 -0700

    Add files via upload

--- Counter.java
+++ Counter.java
@@ -1,37 +1,43 @@
 /*
- *   The counter class implements a counter object that can take a given minimum and maximum,
- *   starting the value at the minimum and increasing or decreasing whenever increase or decrease method is called,
- *   and can tell when its value has rolled over the minimum or maximum.
- *   Group Name: "Better Call Stack"
- *   Group Members:  Linh Pham, Huiguang Ma, Jaya Singh and Vincent Xayasak.
+ * Group Name: "Better Call Stack"
+ * Group Members: Linh Pham, Huiguang Ma, Jaya Singh and Vincent Xayasak
+ * Instructor: Mirsaeid Abolghasemi
+ * Date: 4/20/23
+ * 
+ * The counter class itself is a custom data structure representing a counter that can take a given minimum and maximum,
+ * starting the value at the minimum and increasing or decreasing whenever the increase or decrease method is called,
+ * and can tell when its value has rolled over the minimum or maximum. 
  */
 public class Counter
 {
     private int min;
     private int max;
     private int val;
+    private int previousVal;
 
     public Counter()
     {
         this.min = 0;
         this.max = Integer.MAX_VALUE;
         this.val = 0;
+        this.previousVal = this.val;
     }
 
     public Counter(int min, int max)
     {
-        if(min == max) {
+        if(min >= max) {
             throw new CounterInitializationException("Minimum and Maximum cannot be the same!");
         }
         this.min = min;
         this.max = max;
         this.val = min;
+        this.previousVal = this.val;
     }
 
     public boolean equals(Object otherObject)
     {
         if(otherObject instanceof Counter otherCounter){
-            return this.val == otherCounter.val && this.min == otherCounter.min && this.max == otherCounter.max;
+            return this.val == otherCounter.val && this.min == otherCounter.min && this.max == otherCounter.max && this.previousVal == otherCounter.previousVal;
         }
         return false;
     }
@@ -39,11 +45,19 @@ public class Counter
     public void increase()
     {
         this.val++;
+        this.previousVal = this.val;
+        if(rolledOver()){
+            this.val = this.min;
+        }
     }
 
     public void decrease()
     {
         this.val--;
+        this.previousVal = this.val;
+        if(rolledOver()){
+            this.val = this.max;
+        }
     }
 
     public int value()
@@ -53,11 +67,14 @@ public class Counter
 
     public boolean rolledOver()
     {
-        return this.val == this.max || this.val == this.min;
+        if(this.previousVal > this.max || this.previousVal < this.min){
+            return true;
+        }
+        return false;
     }
 
     public String toString()
     {
-        return "A Counter object that keeps track of the amount of clicks as values, incrementing or decrementing.";
+        return "Counter object with current value of "+this.val+", minimum of "+this.min+", and maximum of "+this.max+".\nRolled Over: "+rolledOver();
     }
 }
